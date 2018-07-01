abacd40c1cd347b3eaa996dde74b5feb689b1dec
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-06-30 19:57:20 -0700

    Added les_miserables_most dataset to data update demo

--- demos/static/eusocial_update_data.js
+++ demos/static/eusocial_update_data.js
@@ -11,13 +11,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function update_data() {
-    CUR_DATA = ((CUR_DATA + 1) % 3);
+    CUR_DATA = ((CUR_DATA + 1) % 4);
 
     if (CUR_DATA == 0) {
         viz.data(les_miserables_data);
     }
     else if (CUR_DATA == 1) {
-        viz.data(small_les_miserables_data);
+        viz.data(les_miserables_most_data);
+    }
+    else if (CUR_DATA == 2) {
+        viz.data(les_miserables_small_data);
     }
     else {
         viz.data(empty_data);
