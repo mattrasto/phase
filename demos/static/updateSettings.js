bc42f52eef52e8dd57ed40ab883e0d37e92de9a8
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-07-27 20:42:56 -0700

    Refactored settings demo

--- demos/static/updateSettings.js
+++ demos/static/updateSettings.js
@@ -13,47 +13,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 });
 
-function toggleZoom() {
-    if (viz.settings().zoom) {
-        viz.settings({zoom: false});
-    }
-    else {
-        viz.settings({zoom: true});
-    }
-}
-
-function changeGravity(newVal) {
-    viz.settings({gravity: newVal});
-}
-
-function changeCharge(newVal) {
-    viz.settings({charge: newVal});
-}
-
-function changeLinkStrength(newVal) {
-    viz.settings({linkStrength: newVal});
-}
-
-function changeLinkDistance(newVal) {
-    viz.settings({linkDistance: newVal});
-}
-
-function changeNodeSize(newVal) {
-    viz.settings({nodeSize: newVal});
-}
-
-function changeLinkWidth(newVal) {
-    viz.settings({linkWidth: newVal});
-}
-
-function changeNodeColor(newVal) {
-    viz.settings({nodeColor: newVal});
-}
-
-function changeNodeBorderColor(newVal) {
-    viz.settings({nodeBorderColor: newVal});
-}
-
-function changeLinkColor(newVal) {
-    viz.settings({linkColor: newVal});
+function changeSettings(prop, val) {
+    viz.settings({[prop]: val});
 }
