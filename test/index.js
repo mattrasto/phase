302aab82eb23185c5d4a60c266c971d674fa2ad3
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-12-31 17:41:13 -0600

    Test for existence of nodes in basic demo

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-const {Builder} = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-const assert = require('assert');
+const assert  = require('chai').assert;
+const expect  = require('chai').expect;
 
 describe('Phase Google Chrome test suite', function() {
 
@@ -9,11 +10,31 @@ describe('Phase Google Chrome test suite', function() {
 
     const pathToFile = file => `localhost:8000/demos/${file}.html`;
 
-    it('should open up a webpage and check the title', async function() {
+    it('should open basic demo and check the title', async function() {
         // Basic demo should load within 3 seconds
         await driver.manage().setTimeouts({pageLoad: 3000});
         await driver.get(pathToFile('basic'));
+
         const title = await driver.getTitle();
-        assert.equal(title, 'Basic Demo - Phase');
+        expect(title, 'Title is correct').to.equal('Basic Demo - Phase');
+    });
+
+    it('should open basic demo and check that nodes exist', async function() {
+        // Wait until network is rendered
+        let svg = await driver.findElement(By.id('phase-network'));
+        await driver.wait(until.elementIsVisible(svg), 100);
+
+        // Get text elements (labels) in node elements
+        let nodes = await svg.findElements(By.css('.node'));
+        let nodePromises = nodes.map(function(node) {
+            return node.getText();
+        });
+        Promise.all(nodePromises)
+            .then(function(nodeNames) {
+                expect(nodeNames, 'nodes exist').to.include.members(['Valjean', 'Gribier', 'Boulatruelle']);
+            })
+            .catch(function(rej) {
+                console.info(rej);
+            });
     });
 });
