afa90f622d3117087b8220860974c0bd61a03c28
Matthew Rastovac <matthewrastovac@yahoo.com>
2018-12-31 15:35:14 -0600

    Configure mocha/selenium timeouts

--- test/index.js
+++ test/index.js
@@ -2,13 +2,16 @@ const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const assert = require('assert');
 
-describe('Phase test suite', function() {
+describe('Phase Google Chrome test suite', function() {
 
     const driver = new Builder().forBrowser('chrome').build();
+    driver.manage().setTimeouts({implicit: 5000, pageLoad: 5000, script: 5000})
 
     const pathToFile = file => `localhost:8000/demos/${file}.html`;
 
     it('should open up a webpage and check the title', async function() {
+        // Basic demo should load within 3 seconds
+        await driver.manage().setTimeouts({pageLoad: 3000});
         await driver.get(pathToFile('basic'));
         const title = await driver.getTitle();
         assert.equal(title, 'Basic Demo - Phase');
