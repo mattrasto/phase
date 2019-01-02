cb3e93830a44a53b642e6c35ce73de8fd90e463b
Matthew Rastovac <matthewrastovac@yahoo.com>
2019-01-01 19:35:28 -0600

    Test node styles in array grouping demo

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const expect  = require('chai').expect;
 describe('Phase Google Chrome test suite', function() {
 
     const pathToFile = file => `localhost:8000/demos/${file}.html`;
-    
+
     const driver = new Builder().forBrowser('chrome').build();
 
     before(function() {
@@ -44,5 +44,26 @@ describe('Phase Google Chrome test suite', function() {
             .catch(function(rej) {
                 console.info(rej);
             });
+    });
+
+    it('should open array grouping demo and check that group styles are applied', async function() {
+        // Basic demo should load within 3 seconds
+        await driver.manage().setTimeouts({pageLoad: 3000});
+        await driver.get(pathToFile('array_grouping'));
+
+        // Wait until network is rendered
+        let svg = await driver.findElement(By.id('phase-network'));
+        await driver.wait(until.elementIsVisible(svg), 100);
+
+        // Apply morphs
+        let applyMorphButton = await driver.findElement(By.css('#sidebar input'));
+        applyMorphButton.click();
+
+        // Check that "Myriel" node has correct styles
+        let myrielNode = await svg.findElement(By.xpath('//*[name()="g" and @class="node"]/*[name()="text" and contains(text(), "Myriel")]/../*[name()="circle"]'));
+        let myrielNodeFill = await myrielNode.getCssValue('fill');
+        expect(myrielNodeFill, 'Myriel node is filled blue').to.equal('rgb(125, 171, 255)');
+        let myrielNodeStroke = await myrielNode.getCssValue('stroke');
+        expect(myrielNodeStroke, 'Myriel node is bordered purple').to.equal('rgb(174, 99, 212)');
     });
 });
