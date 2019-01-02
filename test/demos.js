c1b64c230de097541667a72b0dcb2ccbf53b6778
Matthew Rastovac <matthewrastovac@yahoo.com>
2019-01-01 22:16:57 -0600

    Add and refactor tests for basic demo demo network rendering

--- test/demos.js
+++ test/demos.js
@@ -8,10 +8,25 @@ const pathToFile = file => `localhost:8000/demos/${file}.html`;
 fixture('Basic')
     .page(pathToFile('basic'));
 
-test('Test network rendering', async t => {
+test('Test network rendering timeout', async t => {
         // Basic demo should load within 3 seconds
         await t.setPageLoadTimeout(3000);
 
+        // Wait for network to render
+        await Selector('#phase-network', {visibilityCheck: true});
+});
+
+test('Test viz container exists', async t => {
+        // Check that viz container has a child element
+        const vizContainer = await Selector('#viz-container', {visibilityCheck: true});
+        await t.expect(vizContainer.hasChildNodes).ok('Viz container does not have any child elements');
+
+        // Check that SVG element is visible
+        await t.expect(Selector('#phase-network').visible).ok('Network container SVG element is not visible');
+});
+
+
+test('Test container group classes', async t => {
         // Wait for network to render
         await Selector('#phase-network', {visibilityCheck: true});
 
@@ -19,18 +34,23 @@ test('Test network rendering', async t => {
         const nodeContainerGroup = await Selector('#phase-network > g > g:nth-child(2)');
         await t.expect(nodeContainerGroup.classNames).contains('nodes', 'Node container group does not have "nodes" class');
 
+        // Check that link container group has proper class
+        const linkContainerGroup = await Selector('#phase-network > g > g:nth-child(1)');
+        await t.expect(linkContainerGroup.classNames).contains('links', 'Link container group does not have "links" class');
+});
+
+test('Test container classes', async t => {
+        // Wait for network to render
+        await Selector('#phase-network', {visibilityCheck: true});
+
         // Check that node container has proper class
         const nodeContainer = await Selector('#phase-network > g > g.nodes > g:nth-child(1)');
         await t.expect(nodeContainer.classNames).contains('node', 'Node container does not have "node" class');
 
-        // Check that link container group has proper class
-        const linkContainerGroup = await Selector('#phase-network > g > g:nth-child(1)');
-        await t.expect(linkContainerGroup.classNames).contains('links', 'Link container group does not have "links" class');
-
         // Check that link container has proper class
         const linkContainer = await Selector('#phase-network > g > g.links > g:nth-child(1)');
         await t.expect(linkContainer.classNames).contains('link', 'Link container does not have "link" class');
-    });
+});
 
 
 fixture('Array Grouping')
@@ -46,7 +66,7 @@ test('Test node style morphs', async t => {
         const myrielNode = await Selector('#phase-network > g > g.nodes > g:nth-child(1) > circle');
         await t.expect(myrielNode.getStyleProperty('fill')).eql('rgb(125, 171, 255)', 'Node does not have proper fill');
         await t.expect(myrielNode.getStyleProperty('stroke')).eql('rgb(174, 99, 212)', 'Node does not have proper stroke');
-    });
+});
 
 test('Test link style morphs', async t => {
         //Wait for network to render
@@ -58,4 +78,4 @@ test('Test link style morphs', async t => {
         const link = await Selector('#phase-network > g > g.links > g:nth-child(18) > line');
         await t.expect(link.getStyleProperty('stroke')).eql('rgb(212, 99, 99)', 'Link does not have proper stroke');
         await t.expect(link.getStyleProperty('stroke-width')).eql('3px', 'Link does not have proper stroke-width');
-    });
+});
