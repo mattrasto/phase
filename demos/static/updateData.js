8522640e23e8cd5e3b737a8713736a9d0b724198
Akshay Chalana <ac2zoom@yahoo.com>
2019-01-04 14:49:08 -0800

    Finished linting demos

--- demos/static/updateData.js
+++ demos/static/updateData.js
@@ -1,27 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // Initialize network object
-    viz = phase.Network("#viz-container");
-    // Attach some initial data
-    viz.data(lesMiserablesData);
+/* eslint-env browser */
+/* global viz:true phase lesMiserablesData lesMiserablesMostData
+ lesMiserablesSmallData emptyData CUR_DATA:true */
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize network object
+  viz = phase.Network('#viz-container');
+  // Attach some initial data
+  viz.data(lesMiserablesData);
 
-    CUR_DATA = 0;
+  CUR_DATA = 0;
 
-    console.log("Visualization Loaded");
+  console.log('Visualization Loaded');
 });
 
+/* eslint-disable */
 function updateData() {
-    CUR_DATA = ((CUR_DATA + 1) % 4);
+  CUR_DATA = ((CUR_DATA + 1) % 4);
 
-    if (CUR_DATA == 0) {
-        viz.data(lesMiserablesData);
-    }
-    else if (CUR_DATA == 1) {
-        viz.data(lesMiserablesMostData);
-    }
-    else if (CUR_DATA == 2) {
-        viz.data(lesMiserablesSmallData);
-    }
-    else {
-        viz.data(emptyData);
-    }
+  if (CUR_DATA === 0) {
+    viz.data(lesMiserablesData);
+  } else if (CUR_DATA === 1) {
+    viz.data(lesMiserablesMostData);
+  } else if (CUR_DATA === 2) {
+    viz.data(lesMiserablesSmallData);
+  } else {
+    viz.data(emptyData);
+  }
 }
+/* eslint-enable */
