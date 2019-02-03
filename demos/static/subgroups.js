35fd262523a2425342780c1ac935367e8a6d0f7c
Matthew Rastovac <mattrasto@gmail.com>
2019-02-02 18:37:42 -0600

    Implement subgroups demo

--- demos/static/subgroups.js
+++ demos/static/subgroups.js
@@ -8,3 +8,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('Visualization Loaded');
 });
+
+/* eslint-disable */
+function showMammals() {
+  const mammals = viz.nodeGroup('mammals', 'class', 'mammal');
+  mammals.style({'fill': '#ED9A55'});
+  // If friendlyMammals exists, restyle
+  let friendlyMammals = viz.getNodeGroup('friendly_mammals');
+  if (friendlyMammals) friendlyMammals.style({'fill': '#63B2D4'});
+};
+
+function showFriendlyMammals() {
+  // If 'mammals' group exists, make a subgroup
+  const mammals = viz.getNodeGroup('mammals');
+  let friendlyMammals = null;
+  if (mammals) friendlyMammals = mammals.subgroup('friendly_mammals', 'likesHumans', 'true');
+  else {
+    friendlyMammals = viz.nodeGroup('friendly_mammals', (d) => {
+      return d.class === 'mammal' && d.likesHumans === 'true';
+    });
+  }
+  friendlyMammals.style({'fill': '#63B2D4'});
+}
+
+function hideMammals() {
+  // Destroy mammals (parent) group
+  let mammals = viz.getNodeGroup('mammals');
+  if (mammals) mammals.unstyle().destroy();
+  // Restyle friendly mammals group
+  let friendlyMammals = viz.getNodeGroup('friendly_mammals');
+  if (friendlyMammals) friendlyMammals.style({'fill': '#63B2D4'});
+}
+
+function hideFriendlyMammals() {
+  // Destroy friendlyMammals (child) group
+  let friendlyMammals = viz.getNodeGroup('friendly_mammals');
+  if (friendlyMammals) friendlyMammals.unstyle().destroy();
+  // Restyle mammals group
+  let mammals = viz.getNodeGroup('mammals');
+  if (mammals) mammals.style({'fill': '#ED9A55'});
+}
